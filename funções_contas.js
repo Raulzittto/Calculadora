1e7dd2cff6810526adb7cef2efc0fb37f30408b9
Raul <soaresraul22@gmail.com>
2024-07-03 09:56:00 -0300

    feat: foram descritos o que cada função faz

--- funções_contas.js
+++ funções_contas.js
@@ -1,7 +1,8 @@
 var result = 0;
+const prompt = require("prompt-sync")();
 
 function contas(num1, num2) {
-  console.log(`Qual operação deseja realizar entre `+ num1 + ` e ` + num2 + `
+  prompt(`Qual operação deseja realizar entre `+ num1 + ` e ` + num2 + `
     ADIÇÃO ( + )
     SUBTRAÇÃO( - )
     MULTIPLICAÇÃO( * )
@@ -9,26 +10,31 @@ function contas(num1, num2) {
     PORCENTAGEM( % )`);
 }
 
+//Função que realiza a operação de adição
 function adição(valor1, valor2) {
   result = valor1 + valor2;
   return result;
 }
 
+//Função que realiza a operação de subtração
 function subtração(valor1, valor2) {
   result = valor1 - valor2;
   return result;
 }
 
+//Função que realiza a operação de multiplicação
 function multiplicação(valor1, valor2) {
   result = valor1 * valor2;
   return result;
 }
 
+//Função que realiza a operação de divisão
 function divisão(valor1, valor2) {
   result = valor1 / valor2;
   return result;
 }
 
+//Função que realiza a operação de porcentagem
 function porcentagem(valor1, valor2) {
   result = valor1 / 100;
   result = result * valor2;
