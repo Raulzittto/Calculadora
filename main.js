d12f5a978dd9691dafb3a6a1f12949ec9eaac490
Raul <soaresraul22@gmail.com>
2024-07-03 11:10:32 -0300

    debug: código corrigido alguns bugs e melhorado visualmente

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+console.log("_____CALCULADORA BÁSICA_____\n");
+
 const prompt = require("prompt-sync")();
 const funcoes = require("./funções_contas.js");
 
 var num1 = prompt("Digite o primeiro valor: ");
 var num2 = prompt("Digite o segunda o valor: ");
 
-//Transforma a string em Number
+// Transforma a string em Number
 num1 = Number(num1);
 num2 = Number(num2);
 
-//Verifica se de fato foram digitados dois números
+// Verifica se de fato foram digitados dois números
 while (isNaN(num1) || isNaN(num2)) {
   console.log("\nValores inválidos");
   if (isNaN(num1)) {
@@ -21,9 +23,10 @@ while (isNaN(num1) || isNaN(num2)) {
 // Mostra o menu de escolha das operações
 funcoes.contas(num1, num2);
 
+// Escolha de operação
 var escolha_conta = prompt("\nDigite sua resposta: ");
 
-//Verifica se a operação escolhida é valida
+// Verifica se a operação escolhida é valida
 var operacoesValidas = [
   "+",
   "adição",
@@ -47,31 +50,41 @@ while (!operacoesValidas.includes(escolha_conta)) {
   escolha_conta = prompt("\nDigite sua resposta: ");
 }
 
+// Realiza a chamada das funções e faz as contas
 switch (escolha_conta) {
   case "+":
   case "adição":
   case "ADIÇÃO":
+    console.log(num1 + " mais " + num2 + " será: ");
     console.log(funcoes.adição(num1, num2));
     break;
   case "-":
   case "subtração":
   case "SUBTRAÇÃO":
+    console.log(num1 + " menos " + num2 + " será: ");
     console.log(funcoes.subtração(num1, num2));
     break;
   case "*":
   case "multiplicação":
   case "MULTIPLICAÇÃO":
-    console·log(funcoes.multiplicação(num1, num2));
+    console.log(num1 + " multiplicado por " + num2 + " será: ");
+    console.log(funcoes.multiplicação(num1, num2));
     break;
   case "/":
   case "divisão":
   case "÷":
   case "DIVISÃO":
+    if (num2 == 0) {
+      console.log("matematicamente impossível realizar uma divisão por 0");
+      break;
+    }
+    console.log(num1 + " dividido por " + num2 + " será: ");
     console.log(funcoes.divisão(num1, num2));
     break;
   case "%":
   case "porcentagem":
   case "PORCENTAGEM":
+    console.log(num1 + "% de " + num2 + " será:");
     console.log(funcoes.porcentagem(num1, num2));
     break;
 }
