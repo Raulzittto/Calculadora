6675e28880a8cf55b2a30857aace1ca4bc2078ba
Raul <soaresraul22@gmail.com>
2024-07-03 08:55:21 -0300

    feat: adicionadas as funções que realizam as contas

---
+++ funções_contas.js
@@ -0,0 +1,38 @@
+var result = 0;
+
+function adição(valor1, valor2) {
+  result = valor1 + valor2;
+  return result;
+}
+
+function subtração(valor1, valor2) {
+  result = valor1 - valor2;
+  return result;
+}
+
+function multiplicação(valor1, valor2) {
+  result = valor1 * valor2;
+  return result;
+}
+
+function divisão(valor1, valor2) {
+  result = valor1 / valor2;
+  return result;
+}
+
+function porcentagem(valor1, valor2) {
+  result = valor1 / 100;
+  result = result * valor2;
+  return result;
+}
+
+module.exports = {
+  adição,
+  subtração,
+  multiplicação,
+  divisão,
+  porcentagem,
+};
+
+
+console.log(porcentagem( 50, 100))
